3dd9696462140c95c7fa4f92da7f23145a6ba0d1
Yutian Chen <scyyc1@gmail.com>
2023-08-13 16:08:33 +0100

    222

--- Week 15/script.py
+++ Week 15/script.py
@@ -1,17 +1,50 @@
 # Packages
-import numpy as np
-import matplotlib.pyplot as plt
+import time
 
 # Self-defined functions
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join('..')))
-from util.mesh.triangle.algorithm.R2.mapping_poly_square import Chen_2023_scipy
+from util.mesh.triangle.algorithm.R2.my_test import boundary_smoothing_tutte, poly_square_tutte
 
 from util.pyvista_util import preprocess, postprocess
 
 tritess_v, tritess_f = preprocess("./mesh/cat/cat_input.ply")
-tritess_test = Chen_2023_scipy(tritess_v, tritess_f)
-tritess_test.optimize_default()
-tritess_test.mapping()
-postprocess("./mesh/cat/cat_output2.ply", tritess_test.solution, tritess_f)
+
+# Poly square mapping
+tritess_test1 = poly_square_tutte(tritess_v.copy, tritess_f.copy())
+
+# Boundary Deformation
+start_time = time.time()
+tritess_test1.optimize_default()
+end_time = time.time()
+
+t_boundary_deformation1 = end_time - start_time
+
+# Inner vertices deformation
+start_time = time.time()
+tritess_test1.mapping()
+end_time = time.time()
+
+t_tutte1 = end_time - start_time
+
+postprocess("./mesh/cat/cat_out_PolySquare.ply", tritess_test1.solution, tritess_f)
+
+# Boundary smoothing
+tritess_test2 = poly_square_tutte(tritess_v.copy, tritess_f.copy())
+
+# Boundary Deformation
+start_time = time.time()
+tritess_test2.optimize_default()
+end_time = time.time()
+
+t_boundary_deformation2 = end_time - start_time
+
+# Inner vertices deformation
+start_time = time.time()
+tritess_test2.mapping()
+end_time = time.time()
+
+t_tutte2 = end_time - start_time
+
+postprocess("./mesh/cat/cat_out_PolySquare.ply", tritess_test2.solution, tritess_f)
--- util/mesh/triangle/algorithm/R2/my_test.py
+++ util/mesh/triangle/algorithm/R2/my_test.py
@@ -47,7 +47,7 @@ class poly_square_tutte:
             L2 = np.linalg.norm(edge2)
             
             cos_theta = np.dot(edge1, edge2) / (L1 * L2)
-            if cos_theta < 0:
+            if cos_theta > 0:
                 angle = np.arccos(np.clip(cos_theta, -1.0, 1.0))
                 E_angle += self.activation(angle)
             
@@ -109,9 +109,9 @@ class poly_square_tutte:
         plt.legend()
         if save_dict:
             plt.savefig(save_dict, dpi=300)
-        plt.show()
+        plt.show() 
         
-class edge_smoothing_tutte:
+class boundary_smoothing_tutte:
     def __init__(self, vertices, faces, lambda1=1, lambda2=1, max_iter = 100):
         self.max_iter = max_iter
         self.v_num = vertices.shape[0]
@@ -136,14 +136,12 @@ class edge_smoothing_tutte:
         if angle < (np.pi/2):
             return np.power(np.cos(angle), 2) / angle
         else:
-            return 0
+            return np.power(np.cos(angle), 2)
         
     def objective(self, BV):
         BV = BV.reshape((self.BV_num,2))
 
-        E_align = 0
         E_angle = 0
-        
         for i, (v1, v2) in enumerate(self.BV_r_BV):
             edge1 = BV[v1] - BV[i]
             edge2 = BV[v2] - BV[i]
@@ -152,13 +150,10 @@ class edge_smoothing_tutte:
             L2 = np.linalg.norm(edge2)
             
             cos_theta = np.dot(edge1, edge2) / (L1 * L2)
-            if cos_theta < 0:
-                angle = np.arccos(np.clip(cos_theta, -1.0, 1.0))
-                E_angle += self.activation(angle)
-            
-            E_align += L1*(np.sum(np.absolute(edge1/L1)) - 1) + L2*(np.sum(np.absolute(edge2/L2)) - 1)
+            angle = np.arccos(np.clip(cos_theta, -1.0, 1.0))
+            E_angle += self.activation(angle)
 
-        return E_angle + self.lambda1*E_align
+        return E_angle
     
     def mapping(self):
         self.solution = Tutte_embedding_2D(self.vertices, self.faces, self.solution[self.BV_r_V])
